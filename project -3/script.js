828e75aa0412d1a2b4d259211adefa2c90d3a6d4
Gokul P <balamanibalamani452@gmail.com>
2025-07-17 14:43:55 +0530

    vignesh

---
+++ project -3/script.js
@@ -0,0 +1,17 @@
+const hours=document.querySelector('.hrs');
+const minute=document.querySelector('.min');
+const second=document.querySelector('.sec');
+
+function runclock(){
+    const time=new Date();
+    const sec=time.getSeconds()/60;
+    const min=(sec+time.getMinutes())/60;
+    const hrs=(min+time.getHours())/12;
+    hours.style.setProperty('--rotation',hrs*360);
+    minute.style.setProperty('--rotation',min*360);
+    second.style.setProperty('--rotation',sec*360);
+
+
+};
+runclock();
+setInterval(runclock,1000);
